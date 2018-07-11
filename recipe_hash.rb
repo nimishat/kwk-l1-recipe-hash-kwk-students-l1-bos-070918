5404e55cbc5410c864d1676c255ce100be54b250
nimishat <nimishathakkar83@gmail.com>
2018-07-11 18:53:55 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -4,15 +4,15 @@
 perfect_10_recipe = {}
 perfect_10_recipe = 
 :almond flour => "2 cups"
-, :gluten freewhole oats => "1 cup"
-, :kosher salt => "1/2 tea spoon"
-, :baking powder => "1/2 tea spoon"
-, :baking soda => "1/4 tea spoon"
-, :Xanthan Gum =>"1/2 tea spoon"
-, :slivered almonds, lightly toasted => "1/4 cup"
-, :mini dark chocolate chips => "3/4 cup"
-, :olive oil => "1/2 cup"
-, :agave => "1/4 cup"
+,:gluten freewhole oats => "1 cup"
+,:kosher salt => "1/2 tea spoon"
+,:baking powder => "1/2 tea spoon"
+,:baking soda => "1/4 tea spoon"
+,:Xanthan Gum =>"1/2 tea spoon"
+,:slivered almonds, lightly toasted => "1/4 cup"
+,:mini dark chocolate chips => "3/4 cup"
+,:olive oil => "1/2 cup"
+,:agave => "1/4 cup"
 
 
 
