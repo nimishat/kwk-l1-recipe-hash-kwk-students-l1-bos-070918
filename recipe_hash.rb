4df8669ffe2b76919ae21c874129179ae939272a
nimishat <nimishathakkar83@gmail.com>
2018-07-11 18:58:36 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -3,12 +3,8 @@
 #1. Create a hash called `perfect_10_recipe` that has the following data:
 
 perfect_10_recipe = {
-:almond_flour => "2 cups",:gluten_freewhole_oats => "1 cup",:kosher_salt => "1/2 tea spoon",:baking_powder => "1/2 tea spoon"
-,:baking_soda => "1/4 tea spoon",:Xanthan_Gum =>"1/2 tea spoon"
-,:slivered_almonds_lightly_toasted => "1/4 cup"
-,:mini_dark_chocolate_chips => "3/4 cup"
-,:olive_oil => "1/2 cup"
-,:agave => "1/4 cup"
+  :almond_flour => "2 cups",:gluten_freewhole_oats => "1 cup",:kosher_salt => "1/2 tea spoon",:baking_powder => "1/2 tea spoon"
+,:baking_soda => "1/4 tea spoon",:Xanthan_Gum =>"1/2 tea spoon",:slivered_almonds_lightly_toasted => "1/4 cup",:mini_dark_chocolate_chips => "3/4 cup",:olive_oil => "1/2 cup",:agave => "1/4 cup"
 }
 
 
