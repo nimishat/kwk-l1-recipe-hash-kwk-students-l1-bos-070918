46124de78c607685a9d131b351f0b1a16f6f425a
nimishat <nimishathakkar83@gmail.com>
2018-07-11 19:03:39 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -4,8 +4,7 @@
 
 perfect_10_recipe = {
   :almond_flour => "2 cups",:gluten_freewhole_oats => "1 cup",:kosher_salt => "1/2 tea spoon",:baking_powder => "1/2 tea spoon"
-,:baking_soda => "1/4 tea spoon",:Xanthan_Gum =>"1/2 tea spoon",:slivered_almonds_lightly_toasted => "1/4 cup",:mini_dark_chocolate_chips => "3/4 cup",:olive_oil => "1/2 cup",:agave => "1/4 cup"
-}
+,:baking_soda => "1/4 tea spoon",:Xanthan_Gum =>"1/2 tea spoon",:slivered_almonds_lightly_toasted => "1/4 cup",:mini_dark_chocolate_chips => "3/4 cup",:olive_oil => "1/2 cup",:agave => "1/4 cup" }
 
 
 
